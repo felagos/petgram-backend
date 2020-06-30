621e7f67df5939781c2019561c4570d4221b6213
Felipe Lagos <felagossanchez@gmail.com>
2020-06-29 20:50:35 -0400

    model para retorno de datos

--- src/controllers/categoria.controller.ts
+++ src/controllers/categoria.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { HttpStatus } from "@enums/http.enum";
 import CategoriaService from "@services/categoria.service";
+import { ResponseData } from "@models/response.model";
 
 class CategoriaController {
 
   public async getAll(req: Request, res: Response): Promise<Response> {
     const response = await CategoriaService.getAll();
-    return res.status(HttpStatus.OK).json({ data: response });
+    return res.status(HttpStatus.OK).json(new ResponseData(response));
   }
 
 }
--- src/controllers/mascota.controller.ts
+++ src/controllers/mascota.controller.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import { HttpStatus } from "@enums/http.enum";
 import MascotaService from "@services/mascota.service";
+import { ResponseData } from "@models/response.model";
 
 class MascotaController {
 
     public async getMascotaByCategoriId(req: Request, res: Response): Promise<Response> {
         const { categoriaId } = req.params;
         const response = await MascotaService.getMascotaByCategoriId(categoriaId);
-        return res.status(HttpStatus.OK).json({ data: response });
+        return res.status(HttpStatus.OK).json(new ResponseData(response));
     }
 
 }
