8f3476c1945d82a7223e55728ecee8ee3f4a4160
Felipe Lagos <felagossanchez@gmail.com>
2020-11-22 17:52:54 -0300

    add log mongo connection

--- src/mongo/connection.ts
+++ src/mongo/connection.ts
@@ -4,10 +4,12 @@ import { environment } from '@env';
 export class MongoDB {
 
     public static connect(): void {
+        console.log('mongo uri', String(environment.MONGODB_URI));
         mongoose.connect(String(environment.MONGODB_URI), { useNewUrlParser: true })
             .then(() => {
                 console.log("Mongo connected");
-            }).catch(() => {
+            }).catch((e) => {
+                console.log('error mongo', e);
                 console.log('Mongodb connection failed.');
             })
     }
