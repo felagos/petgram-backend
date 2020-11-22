0e0e09812a96570d7b781f841ad67794edf6ad58
Felipe Lagos <felagossanchez@gmail.com>
2020-11-22 17:56:49 -0300

    add log

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import "./alias";
 import App from '@app';
 import { environment } from '@env';
 
+console.log("env", environment);
+
 App.listen(environment.PORT, () => {
     console.log(`Server up ${environment.PORT} !`);
 });
